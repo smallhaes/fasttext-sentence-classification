07131aa744c7ef64f5b7d23eaa8ca7c7a0bce46f
Hinata <littlehaes@bupt.edu.cn>
2020-06-19 10:01:59 +0800

    pipeline for fasttext

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,5 @@
 echo 'pythonenv3.7' >> .gitignore
-pythonenv3.7/bin/pip install  --extra-index-url=https://azuremlsdktestpypi.azureedge.net/CLI-SDK-Runners-Validation/15587761 -r requirements.txt
+echo 'get-pip.py' >> .gitignore
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+python3 -m pip install  --extra-index-url=https://azuremlsdktestpypi.azureedge.net/CLI-SDK-Runners-Validation/15587761 -r requirements.txt
