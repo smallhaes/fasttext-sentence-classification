63a0ba9e215085af4f1531f2415a9a266d8c92e7
LittleSasuke <506575628@qq.com>
2020-06-22 10:02:57 +0800

    Delete prepare.sh

--- prepare.sh
+++
@@ -1,5 +0,0 @@
-echo 'pythonenv3.7' >> .gitignore
-echo 'get-pip.py' >> .gitignore
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-python3 -m pip install  --extra-index-url=https://azuremlsdktestpypi.azureedge.net/CLI-SDK-Runners-Validation/15886596 -r requirements.txt
