8d92c0bcf6bb12d2bf229bfc592b953d410f77cc
Hinata <t-yangx@microsoft.com>
2020-08-12 11:50:37 +0000

    update

--- deployment/scoring_for_deployment.py
+++ deployment/scoring_for_deployment.py
@@ -4,12 +4,16 @@ import torch
 from inference_schema.schema_decorators import input_schema, output_schema
 from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
 
-from common.utils import load_dataset, DataIter, predict, get_vocab, get_id_label
+from common.utils import load_dataset_for_deployment, DataIter, predict, get_vocab, get_id_label
 
 
 def init():
-    global model, word_to_index, map_label_id
+    global model, word_to_index, map_label_id, map_id_label, device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model_dir = os.getenv('AZUREML_MODEL_DIR')
+    # Model.register() will upload the parent directory, so we need to accommodate this direcotry in model_dir
+    # The name of this parent directory is the output port name.
+    model_dir = os.path.join(model_dir, os.listdir(model_dir)[0])
     path_word_to_index = os.path.join(model_dir, 'word_to_index.json')
     word_to_index = get_vocab(path_word_to_index)
     path_label = os.path.join(model_dir, 'label.txt')
@@ -17,6 +21,7 @@ def init():
     model_name = 'BestModel'
     model_path = os.path.join(model_dir, model_name)
     model = torch.load(f=model_path)
+    
 
 
 standard_sample_input = {'input_sentence': 'i want to travel around the world'}
@@ -27,14 +32,12 @@ standard_sample_output = {'category': 'dream'}
 @output_schema(StandardPythonParameterType(standard_sample_output))
 def run(param):
     with torch.no_grad():
-        path = param['input_sentence']
         max_len_ = 32
-        test_samples = load_dataset(file_path=path, max_len=max_len_, word_to_index=word_to_index,
-                                    map_label_id=map_label_id)
+        test_samples = load_dataset_for_deployment(input_sentence=param['input_sentence'], max_len=max_len_, 
+                                                   word_to_index=word_to_index)
         test_iter = DataIter(test_samples, batch_size=1)
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         try:
-            result = predict(model, test_iter, device)
+            result = predict(model, test_iter, device, map_id_label)
             # You can return any data type, as long as it is JSON serializable.
             return result
         except Exception as e:
