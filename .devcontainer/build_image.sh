cde335ac9c7e654854891c1d1ef1c53552d0157e
Xiaoyu Yang <t-yangx@microsoft.com>
2020-08-14 23:16:00 +0800

    update

--- .devcontainer/build_image.sh
+++ .devcontainer/build_image.sh
@@ -1,11 +1,11 @@
 # If you want to create your own image. Modify the Dockerfile. Then run this shell script: bash build_image.sh
-# login to your docker hub
-docker login
 # input the username of your docker hub
-echo -e '\nPlease input the username of your docker hub again for later push: '
+echo -e '\nPlease input the username of your docker hub'
 echo -e 'Username: \c'
 read line
 USERNAME=$line
+# login to your docker hub
+docker login -u ${USERNAME}
 # get the lataest version number of azureml sdk as the image tag
 SDK_VERSION_SHORT=`curl -s https://modulesdkpreview.blob.core.windows.net/sdk/preview/version.txt`
 SDK_VERSION_SHORT=${SDK_VERSION_SHORT:0:8}
@@ -13,6 +13,9 @@ SDK_VERSION_SHORT=${SDK_VERSION_SHORT:0:8}
 REPOSITORY_NAME=azureml-sdk
 # the complete name of the image with a tag
 TARGET_NAME=${USERNAME}/${REPOSITORY_NAME}:$SDK_VERSION_SHORT
+LATEST_NAME=${USERNAME}/${REPOSITORY_NAME}:latest
 # build an image from a Dockerfile
 docker build -t ${TARGET_NAME} .
 docker push ${TARGET_NAME}
+docker tag ${TARGET_NAME} ${LATEST_NAME}
+docker push ${LATEST_NAME}
