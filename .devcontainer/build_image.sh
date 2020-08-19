920bfb48e59440f77d28bad4dceb9362f4e5479b
Xiaoyu Yang <t-yangx@microsoft.com>
2020-08-19 22:41:16 +0800

    env

--- .devcontainer/build_image.sh
+++ .devcontainer/build_image.sh
@@ -2,23 +2,28 @@
 # We highly recommend to use image instead of Dockerfile in 'devcontainer.json'
 # Because pulling an image is much faster than building an image
 
-# input the username of your docker hub
+# Input the username of your docker hub
 echo -e '\nPlease input the username of your docker hub'
 echo -e 'Username: \c'
 read line
 USERNAME=$line
-# login to your docker hub
+# Login to your docker hub
 docker login -u ${USERNAME}
-# get the lataest version number of azureml sdk as the image tag
+# Get the lataest version number of azureml sdk as the image tag
 SDK_VERSION_SHORT=`curl -s https://modulesdkpreview.blob.core.windows.net/sdk/preview/version.txt`
 SDK_VERSION_SHORT=${SDK_VERSION_SHORT:0:8}
-# the repository name displayed on docker hub
+# The repository name displayed on docker hub
 REPOSITORY_NAME=azureml-sdk
-# the complete name of the image with a tag
+# The complete name of the image with a tag
 TARGET_NAME=${USERNAME}/${REPOSITORY_NAME}:$SDK_VERSION_SHORT
 LATEST_NAME=${USERNAME}/${REPOSITORY_NAME}:latest
-# build an image from a Dockerfile
+# Build an image from a Dockerfile
 docker build -t ${TARGET_NAME} .
+# Check whether the build process is executed successfully
+if [[ $? != 0 ]]; then
+        echo 'Build failed. Please check your Dockerfile'
+        exit
+fi
 docker push ${TARGET_NAME}
 # update the latest version
 docker tag ${TARGET_NAME} ${LATEST_NAME}
